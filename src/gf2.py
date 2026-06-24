2fce902f8744f9397c8549546781e056228e1675
Frixit <05mba0622@gmail.com>
2026-06-24 09:40:38 +0300

    feat: добавить функции сложения и умножения над GF(2)

---
+++ src/gf2.py
@@ -0,0 +1,21 @@
+import numpy
+
+__all__ = ["gf2_add", "gf2_mul"]
+
+
+def gf2_add(a: numpy.ndarray, b: numpy.ndarray) -> numpy.ndarray:
+    """
+    Сложить два вектора или матрицы над GF(2).
+    """
+    a = numpy.asarray(a, dtype=numpy.int8)
+    b = numpy.asarray(b, dtype=numpy.int8)
+    return (a + b) % 2
+
+
+def gf2_mul(a: numpy.ndarray, b: numpy.ndarray) -> numpy.ndarray:
+    """
+    Перемножить две матрицы (или матрицу и вектор) над GF(2).
+    """
+    a = numpy.asarray(a, dtype=numpy.int8)
+    b = numpy.asarray(b, dtype=numpy.int8)
+    return numpy.mod(a @ b, 2)

6792fd1f51583c837e74ba5fa448359d03f1e7c1
frixit <05mba0622@gmail.com>
2026-07-15 10:22:31 +0300

    refactor: экспортировать публичный API модулей в __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,19 @@
-__all__ = ()
+from src.gf2 import gf2_add, gf2_inv, gf2_mul
+from src.hamming import G, H, build_syndrome_table, correct, encode, syndrome
+from src.mceliece import brute_force, decrypt, encrypt, keygen
+
+__all__ = (
+    "G",
+    "H",
+    "brute_force",
+    "build_syndrome_table",
+    "correct",
+    "decrypt",
+    "encode",
+    "encrypt",
+    "gf2_add",
+    "gf2_inv",
+    "gf2_mul",
+    "keygen",
+    "syndrome",
+)
